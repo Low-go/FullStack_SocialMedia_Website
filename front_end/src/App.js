5118641b946271ce1f82614ec83cbca752ac82fb
lorran <lorranalvesgaldino98@gmail.com>
2024-05-21 22:02:48 -1000

    Initial routes setup, <h1>test in Home

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+
 
 function App() {
   return (
     <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+        </Routes>
+      </Router>
     </>
   );
 }
---
+++ front_end/src/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+
+function Home(){
+    return (
+        <h1>Hello Test</h1>
+    )
+}
+
+export default Home
