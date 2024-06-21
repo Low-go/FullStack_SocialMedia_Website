05f1440586a52efef3d80aabbad5ea03f9fc3bb0
lorran <lorranalvesgaldino98@gmail.com>
2024-06-20 15:07:42 -1000

    fix hydration error, mismatch between server and client side

--- front_end/src/components/Posts/PostForm/PostItem.js
+++ front_end/src/components/Posts/PostForm/PostItem.js
@@ -5,18 +5,17 @@ import React from 'react';
 const PostItem = ({ post }) => {
   return (
     <Flex
-      borderWidth='2px' // Set general border width
+      borderWidth='2px'
       bg='white'
-      borderColor='#00887a' // Set initial border color
+      borderColor='#00887a'
       borderRadius={8}
       p={4}
       boxShadow='md'
-      _hover={{ boxShadow: "lg", borderColor: "#00887a", borderWidth:'3px' }} // Keep hover effect without changing border width
+      _hover={{ boxShadow: "lg", borderColor: "#00887a", borderWidth:'3px' }}
       cursor='pointer'
       onClick={() => console.log('Post selected')}
       minHeight="200px"
-      width={{ base: "100%", sm: "100%", md: "100%", lg: "100%", xl: "100%" }} // Responsive widths
-      // margin="20px auto"
+      width={{ base: "100%", sm: "100%", md: "100%", lg: "100%", xl: "100%" }}
       direction="column"
       fontFamily="'Arial', sans-serif"
     >
--- front_end/src/components/Posts/PostForm/Posts.js
+++ front_end/src/components/Posts/PostForm/Posts.js
@@ -1,20 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import PostItem from './PostItem';
-import { Stack } from '@chakra-ui/react';
+import { Stack, Spinner } from '@chakra-ui/react';
 
 const Posts = () => {
-    const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(true); // Assume loading initially
     const [posts, setPosts] = useState([]);
 
     const getPosts = async () => {
-        setLoading(true);
         try {
             const response = await axios.get('http://localhost:5000/api/posts?sort=-created_at');
-            console.log('Retrieved Posts:', response.data);
             setPosts(response.data.posts); // Ensure that you're setting an array
         } catch (error) {
-            console.log('getPosts error', error.message);
+            console.error('getPosts error', error.message);
         } finally {
             setLoading(false);
         }
@@ -23,6 +21,10 @@ const Posts = () => {
     useEffect(() => {
         getPosts();
     }, []);
+
+    if (loading) {
+        return <Spinner />;
+    }
 
     return (
         <Stack>
